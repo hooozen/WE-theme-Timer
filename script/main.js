d7e83eaa856396bbbf877a5c095f6a769b70b8f2
Hozen <hozen@live.com>
2021-08-06 00:14:44 +0800

    todo(motto): custom motto
    A issuse occur: WE will listening the text property input when every typing

--- script/main.js
+++ script/main.js
@@ -1,9 +1,24 @@
 function $(id) {
   return document.querySelector(id)
-};
+}
+
+defaultMottos = [
+  '不积跬步无以至千里', '北海虽赊，扶摇可接', '欲穷千里目，更上一层楼!', '大鹏一日同风起，扶摇直上九万里',
+  '会当凌绝顶，一览众山小', '驽马十驾，功在不舍', '读书不觉已春深，一寸光阴一寸金', '黄沙百战穿金甲，不破楼兰终不还',
+  '故天将降大任于是人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能',
+  '鹏北海，凤朝阳。又携书剑路茫茫。明年此日青云去，却笑人间举子忙', '天生我材必有用，千金散尽还复来',
+  '穷且益坚，不坠青云之志', '读书之道无他，求其放心而已矣', '学而时习之，不亦说乎', '温故而知新，可以为师矣',
+  '士不可以不弘毅，任重而道远', '路漫漫其修远兮，吾将上下而求索', '知之为知之，不知为不知，是知也',
+  '学而不思则罔，思而不学则殆', '千淘万漉虽辛苦，吹尽狂沙始到金', '不到长城非好汉，屈指行程二万', '雄关漫道真如铁, 而今迈步从头越',
+  '恰同学少年，风华正茂；书生意气，挥斥方遒', '不抚壮而弃秽兮，何不改乎此度？', '天行健，君子以自强不息',
+  '君不见黄河之水天上来，奔流到海不复回！', '君不见高堂明镜悲白发，朝如青丝暮成雪！', '行路难！行路难！多歧路，今安在',
+  '九万里风鹏正举。风休住，蓬舟吹取三山去！', '乘风破浪会有时，直挂云帆济沧海', '愿乘长风破万里浪',
+  '欲渡黄河冰塞川，将登太行雪满山', '自信人生二百年，会当水击三千里', '业精于勤荒于嬉，行成于思毁于随'
+]
 
 window.wallpaperPropertyListener = {
   applyUserProperties: function (properties) {
+    console.log(properties)
     if (properties.title) {
       // Do something with yourproperty
       if (properties.title.value)
@@ -14,12 +29,21 @@ window.wallpaperPropertyListener = {
         window.GOLOBAL.deadline.date = new Date(properties.deadline.value)
       // Do something with anotherproperty
     }
-    refresh()
+    if (properties.addmotto) {
+      if (properties.addmotto.value) {
+        window.GOLOBAL.addMotto(properties.addmotto.value)
+      }
+    }
+    if (properties.usingdefaultmottos) {
+      window.GOLOBAL.usingDefaultMottos = properties.usingdefaultmottos.value
+    }
     // Add more properties here
+    refresh()
   },
 };
 
 function main() {
+  console.log('reload')
 
   window.GOLOBAL = {
     motto: $('#motto'),
@@ -27,20 +51,19 @@ function main() {
     deadline: getDeadline(),
     switchMottoTime: (new Date()).getMinutes(),
     switchBgTime: (new Date()).getMinutes(),
-    mottos: [
-      '不积跬步无以至千里', '北海虽赊，扶摇可接', '欲穷千里目，更上一层楼!', '大鹏一日同风起，扶摇直上九万里',
-      '会当凌绝顶，一览众山小', '驽马十驾，功在不舍', '读书不觉已春深，一寸光阴一寸金', '黄沙百战穿金甲，不破楼兰终不还',
-      '故天将降大任于是人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能',
-      '鹏北海，凤朝阳。又携书剑路茫茫。明年此日青云去，却笑人间举子忙', '天生我材必有用，千金散尽还复来',
-      '穷且益坚，不坠青云之志', '读书之道无他，求其放心而已矣', '学而时习之，不亦说乎', '温故而知新，可以为师矣',
-      '士不可以不弘毅，任重而道远', '路漫漫其修远兮，吾将上下而求索', '知之为知之，不知为不知，是知也',
-      '学而不思则罔，思而不学则殆', '千淘万漉虽辛苦，吹尽狂沙始到金', '不到长城非好汉，屈指行程二万', '雄关漫道真如铁, 而今迈步从头越',
-      '恰同学少年，风华正茂；书生意气，挥斥方遒', '不抚壮而弃秽兮，何不改乎此度？', '天行健，君子以自强不息',
-      '君不见黄河之水天上来，奔流到海不复回！', '君不见高堂明镜悲白发，朝如青丝暮成雪！', '行路难！行路难！多歧路，今安在',
-      '九万里风鹏正举。风休住，蓬舟吹取三山去！', '乘风破浪会有时，直挂云帆济沧海', '愿乘长风破万里浪',
-      '欲渡黄河冰塞川，将登太行雪满山', '自信人生二百年，会当水击三千里', '业精于勤荒于嬉，行成于思毁于随'
-    ],
-    refresh: refresh
+    defaultMottos,
+    customMottos: [],
+    usingDefaultMottos: true,
+    refresh: refresh,
+
+    addMotto(motto) {
+      motto = motto.trim()
+      index = this.defaultMottos.indexOf(motto)
+      if (index !== -1) return void this.defaultMottos.splice(index, 1)
+      index = this.customMottos.indexOf(motto)
+      if (index !== -1) return void this.customMottos.splice(index, 1)
+      this.customMottos.push(motto)
+    }
   };
 
   var body = $('#body');
@@ -76,13 +99,13 @@ function registeServiceWorker() {
 }
 
 function refresh() {
-  window.requestAnimationFrame(freshNumbers);
+  window.requestAnimationFrame(freshNumbers.bind(window, undefined, true));
 }
 
-function freshNumbers() {
+function freshNumbers(_, rightnow = false) {
   var numbers = getTimerNumbers(GOLOBAL.deadline.date);
   rendserNumber(numbers);
-  switchMotto();
+  switchMotto(rightnow);
   switchBg();
   window.requestAnimationFrame(freshNumbers);
 }
@@ -131,13 +154,28 @@ function getTimerNumbers(deadline) {
   }
 };
 
-function switchMotto(debug) {
+function switchMotto(rightnow) {
+  console.log(GOLOBAL)
+  const {
+    usingDefaultMottos,
+    defaultMottos,
+    customMottos
+  } = GOLOBAL
+  console.log(usingDefaultMottos)
+  let mottos = []
+  if (usingDefaultMottos) {
+    mottos = defaultMottos.concat(customMottos)
+  } else {
+    mottos = customMottos
+  }
+  console.log(mottos)
+  if (mottos.length < 1) return
   var minutes = (new Date()).getMinutes();
   // var minutes = (new Date()).getSeconds();
-  if (debug) {} else if (minutes != GOLOBAL.switchMottoTime) return;
-  GOLOBAL.motto.innerText = GOLOBAL.mottos[Math.floor(GOLOBAL.mottos.length * Math.random())];
+  console.log(rightnow)
+  if (!rightnow && minutes != GOLOBAL.switchMottoTime) return;
+  GOLOBAL.motto.innerText = mottos[Math.floor(mottos.length * Math.random())];
   GOLOBAL.switchMottoTime = Math.floor(Math.random() * 60);
-  console.log('switchMottoTime', GOLOBAL.switchMottoTime)
 }
 
 function switchBg(debug) {
